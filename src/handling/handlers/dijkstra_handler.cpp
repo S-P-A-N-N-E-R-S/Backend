9a9defab96234bb0bdc96d315cd5c4a52430403d
Leon Richardt <leon.richardt@gmail.com>
2021-08-26 21:47:01 +0200

    fix(dijkstra_handler): respect ownership of `ogdf::Array`s
    Moving the arrays to prevent copying is invalid in this case since we
    need the arrays to refer to the new graph (the shortest path graph) in
    the response. Thus, we cannot get around copying the attributes that we
    need in the response.

--- src/handling/handlers/dijkstra_handler.cpp
+++ src/handling/handlers/dijkstra_handler.cpp
@@ -20,15 +20,14 @@ dijkstra_handler::dijkstra_handler(std::unique_ptr<abstract_request> request)
 
 std::pair<graphs::ResponseContainer, long> dijkstra_handler::handle()
 {
-    auto graph_message = m_request->take_graph_message();
+    const auto *graph_message = m_request->graph_message();
 
-    auto &graph = graph_message.graph();
-    int start_uid = std::stoi(m_request->graph_attributes().at("startUid"));
+    auto &graph = graph_message->graph();
+    int start_uid = std::stoi(m_request->graph_attributes().at("intAttributes.startUid"));
 
-    const auto *parsed_node_coords = m_request->node_coords();
     const auto *parsed_costs = m_request->edge_costs();
 
-    const auto &origin = graph_message.node(start_uid);
+    const auto &origin = graph_message->node(start_uid);
     ogdf::NodeArray<ogdf::edge> preds;
     ogdf::NodeArray<double> dist;
 
@@ -41,23 +40,28 @@ std::pair<graphs::ResponseContainer, long> dijkstra_handler::handle()
 
     // Build shortest path graph from origin to index
     auto spg = std::make_unique<ogdf::Graph>();
+    auto sp_node_uids = std::make_unique<ogdf::NodeArray<uid_t>>(*spg);
     auto sp_edge_uids = std::make_unique<ogdf::EdgeArray<server::uid_t>>(*spg);
     auto sp_edge_costs = ogdf::EdgeArray<double>(*spg);
+    auto sp_node_coords = ogdf::NodeArray<node_coordinates>(*spg);
 
-    const auto &og_edge_uids = graph_message.edge_uids();
-    const auto og_node_uids = graph_message.node_uids();
+    const auto &og_edge_uids = graph_message->edge_uids();
+    const auto &og_node_uids = graph_message->node_uids();
+    const auto *og_node_coords = m_request->node_coords();
 
-    // To access nodes by uids
-    ogdf::NodeArray<ogdf::node> original_node_to_sp{*spg};
+    ogdf::NodeArray<ogdf::node> original_node_to_sp{graph};
 
     // add all nodes to answer (before adding edges)
-    for (auto n : graph_message.graph().nodes)
+    for (auto n : graph_message->graph().nodes)
     {
-        original_node_to_sp[n] = spg->newNode();
+        const auto sp_node = spg->newNode();
+        original_node_to_sp[n] = sp_node;
+        (*sp_node_uids)[sp_node] = og_node_uids[n];
+        sp_node_coords[sp_node] = (*og_node_coords)[n];
     }
 
     // add edges to answer
-    for (auto n : graph_message.graph().nodes)
+    for (auto n : graph_message->graph().nodes)
     {
         auto preds_edge = preds[n];
         if (preds_edge != nullptr)
@@ -65,20 +69,18 @@ std::pair<graphs::ResponseContainer, long> dijkstra_handler::handle()
             auto source = original_node_to_sp[n];
             auto target = original_node_to_sp[preds_edge->opposite(n)];
             auto newEdge = spg->newEdge(source, target);
-            (*sp_edge_uids)[newEdge] = graph_message.edge_uids()[preds_edge];
-            sp_edge_costs[newEdge] = m_request->edge_costs()->operator[](preds_edge);
+            (*sp_edge_uids)[newEdge] = graph_message->edge_uids()[preds_edge];
+            sp_edge_costs[newEdge] = (*m_request->edge_costs())[preds_edge];
         }
     }
 
-    server::graph_message spgm{std::move(spg),
-                               std::make_unique<ogdf::NodeArray<uid_t>>(std::move(og_node_uids)),
-                               std::move(sp_edge_uids)};
+    server::graph_message spgm{std::move(spg), std::move(sp_node_uids), std::move(sp_edge_uids)};
 
     response_factory rspf;
     return std::make_pair(
-        rspf.build_response(std::unique_ptr<abstract_response>{new generic_response{
-            "dijkstra", &spgm, m_request->node_coords(), &sp_edge_costs, nullptr, nullptr, nullptr,
-            nullptr, nullptr, nullptr, status_code::OK}}),
+        rspf.build_response(std::unique_ptr<abstract_response>{
+            new generic_response{"dijkstra", &spgm, &sp_node_coords, &sp_edge_costs, nullptr,
+                                 nullptr, nullptr, nullptr, nullptr, nullptr, status_code::OK}}),
         ogdf_time);
 }
 
